f8171fe4a1c4c7208f61ef1794b361b336f4b64d
Graeme Fox <ac.graeme@gmail.com>
2022-09-02 09:09:37 -0700

    light client update

--- empty-webapp/index.ts
+++ empty-webapp/index.ts
@@ -1,25 +1,20 @@
-import {
-    ScProvider,
-    WellKnownChain,
-  } from "@polkadot/rpc-provider/substrate-connect";
-  import { ApiPromise } from "@polkadot/api";
-  
-  window.onload = () => {
-    void (async () => {
-      try {
-        const provider = new ScProvider(WellKnownChain.polkadot);
-        
-        await provider.connect();
-        const api = await ApiPromise.create({ provider });
-        await api.rpc.chain.subscribeNewHeads(
-          (lastHeader: { number: unknown; hash: unknown }) => {
-            console.log(
-              `New block #${lastHeader.number} has hash ${lastHeader.hash}`
-            );
-          }
-        );
-      } catch (error) {
-        console.error(<Error>error);
-      }
-    })();
-  };
+import { ScProvider, WellKnownChain } from "@polkadot/rpc-provider/substrate-connect";import { ApiPromise } from "@polkadot/api";
+import jsonParachainSpec from "./statemint.json";
+
+window.onload = () => {
+void (async () => {
+  try {
+    const relayProvider = new ScProvider(WellKnownChain.polkadot);
+    const parachainSpec = JSON.stringify(jsonParachainSpec);
+    const provider = new ScProvider(parachainSpec, relayProvider);
+    
+    await provider.connect();
+    const api = await ApiPromise.create({ provider });
+    await api.rpc.chain.subscribeNewHeads((lastHeader: { number: unknown; hash: unknown }) => {
+      console.log(`New block #${lastHeader.number} has hash ${lastHeader.hash}`);
+    });
+  } catch (error) {
+    console.error(<Error>error);
+  }
+})();
+};
